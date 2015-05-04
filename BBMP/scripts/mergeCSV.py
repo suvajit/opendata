157f06ac69b334349339582e0bc2ecff3b03577c
Suvajit Sengupta <suvajit.sengupta@gmail.com>
2015-05-04 18:35:42 +0530

    Correction made on files

--- BBMP/scripts/mergeCSV.py
+++ BBMP/scripts/mergeCSV.py
@@ -1,7 +1,13 @@
+#
+# Merge two CSV file based on common field
+# here WARD_NO is the common field
+#
+# @author jeet_sen@yahoo.co.in	
+
 import pandas as pd
 
-a = pd.read_csv("../data/Census_2011.csv")
-b = pd.read_csv("../data/bbmpwards.csv")
+a = pd.read_csv("../work/Bangalore_Ward_Population_2011.csv")
+b = pd.read_csv("../work/bbmpwards_old_data.csv")
 b = b.dropna(axis=1)
 merged = a.merge(b, on='WARD_NO')
-merged.to_csv("output.csv", index=False)
+merged.to_csv("../work/bbmp_wards_data_merged.csv", index=False)
--- BBMP/scripts/shp2csv.py
+++ BBMP/scripts/shp2csv.py
@@ -1,7 +1,14 @@
+# 
+# Extract the attribute table of a Shp file into a CSV file
+#
+# @author jeet_sen@yahoo.co.in
+# 
+
 import ogr,csv,sys
 
-shpfile=r'/Users/Suvajit/Documents/GEOGO/ideas/BBMP/data/bbmpwards/bbmpwards.shp' #sys.argv[1]
-csvfile=r'./bbmpwards.csv' #sys.argv[2]
+#Source file
+shpfile=r'../work/bbmpwards_old/bbmpwards.shp' #sys.argv[1]
+csvfile=r'../work/bbmpwards_old_data.csv' #sys.argv[2]
 
 #Open files
 csvfile=open(csvfile,'wb')
@@ -14,6 +21,8 @@ nfields=dfn.GetFieldCount()
 fields=[]
 for i in range(nfields):
     fields.append(dfn.GetFieldDefn(i).GetName())
+#Not addding the geometry of the polygon. 
+#Uncomment below line to add
 #fields.append('kmlgeometry')
 csvwriter = csv.DictWriter(csvfile, fields)
 try:csvwriter.writeheader() #python 2.7+
@@ -22,6 +31,7 @@ except:csvfile.write(','.join(fields)+'\n')
 # Write attributes and kml out to csv
 for feat in lyr:
     attributes=feat.items()
+# Uncomment below to add geometry
 #    geom=feat.GetGeometryRef()
 #    attributes['kmlgeometry']=geom.ExportToKML()
     csvwriter.writerow(attributes)
